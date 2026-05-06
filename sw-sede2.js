6919ed9baaff80b1e92b20c66d00dfb3cb535829
JOX36 <angelvalenciamurcielago@gmail.com>
2026-05-06 12:09:19 -0500

    Add files via upload

---
+++ sw-sede2.js
@@ -0,0 +1,29 @@
+const VERSION = '1';
+const CACHE = 'vaca-sede2-v' + VERSION;
+const FILES = ['./inventario-app.html', './manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request)
+      .then(response => {
+        const clone = response.clone();
+        caches.open(CACHE).then(cache => cache.put(e.request, clone));
+        return response;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
